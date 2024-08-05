9331200468b7ef195a7a3baae22f20a1044b743b
igorrm <igorrodriguesmachado23@gmail.com>
2024-08-05 15:06:42 -0300

    05/08 - desenvolvimeno do back-end

--- back-end/node.js
+++ back-end/node.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
 
 const app = express();
 app.use(express.json()); //por padrao express n usa json, precissa disso
@@ -7,14 +11,26 @@ let port = 3000;
 
 const users = [];
 
-app.post('/ususarios', (req, res) => {
+app.post('/ususarios', async (req, res) => {
 
-    users.push(req.body) //adiciona ao final de uma lista as informações json, para um array vazio
+   await prisma.user.create({
+        data: {
+
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    //adiciona ao final de uma lista as informações json, para um array vazio
     res.status(201).send("Deu certo! ")
 })
 
 
-app.get('/ususarios', (req, res) => {
+app.get('/ususarios', async(req, res) => {
+
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 });
 
